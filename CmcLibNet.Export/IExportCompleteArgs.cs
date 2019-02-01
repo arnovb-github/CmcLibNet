d239459b1b23aa294af3f5c1e63fa15ff3fc77b9
Arno van Boven <arnovanboven@outlook.com>
2019-02-01 14:22:55 +0100

    Moved to its own file

---
+++ CmcLibNet.Export/IExportCompleteArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Vovin.CmcLibNet.Export
+{
+    /// <summary>
+    /// Interface for ExportCompleted, primarily for use with COM Interop.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("8D262E04-9AE5-4C64-968F-7A7AEAA2B58C")]
+    public interface IExportCompleteArgs
+    {
+        /// <summary>
+        /// Rows processed.
+        /// </summary>
+        int Rows { get; }
+    }
+}
