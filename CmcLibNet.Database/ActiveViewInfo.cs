9def8c36e0b8cf0a2a07c1d6b7ba3d88b884e7d8
Arno van Boven <arnovanboven@outlook.com>
2019-03-12 22:53:01 +0100

    Moved IActiveViewInfo to its own file

--- CmcLibNet.Database/ActiveViewInfo.cs
+++ CmcLibNet.Database/ActiveViewInfo.cs
@@ -2,34 +2,6 @@
 
 namespace Vovin.CmcLibNet.Database
 {
-    /// <summary>
-    /// Exposes properties of the currently active Commence view.
-    /// </summary>
-    [ComVisible(true)]
-    [Guid("1B602809-2599-4dac-8BDB-71D0931E909F")]
-    public interface IActiveViewInfo
-    {
-        /// <summary>
-        /// Name of the view.
-        /// </summary>
-        string Name { get; }
-        /// <summary>
-        /// Type of the view.
-        /// </summary>
-        string Type { get; }
-        /// <summary>
-        /// Underlying view category.
-        /// </summary>
-        string Category { get; }
-        /// <summary>
-        /// Current itemname (if view is a detail form).
-        /// </summary>
-        string Item { get; }
-        /// <summary>
-        /// Currently active fieldname (if view is a detail form).
-        /// </summary>
-        string Field { get; }
-    }
 
     /// <summary>
     /// Exposes properties of the currently active Commence view.
