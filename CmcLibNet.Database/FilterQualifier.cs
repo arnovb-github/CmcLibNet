360d90ced53fdfd034f00a9630bb66c1fc1db765
Arno van Boven <arnovanboven@outlook.com>
2019-05-09 22:14:19 +0200

    Added custom attribute FilterValues for use with FilterQualifier

--- CmcLibNet.Database/FilterQualifier.cs
+++ CmcLibNet.Database/FilterQualifier.cs
@@ -4,107 +4,126 @@ namespace Vovin.CmcLibNet.Database
 {
     /// <summary>
     /// Valid qualifiers used in a Commence ViewFilter request.
-    /// We use this enum to check against the values passed in.
-    /// This enum is not exposed to COM because enums cannot be strings,
-    /// we just trick ourselves here.
     /// </summary>
     public enum FilterQualifier
     {
         /// <summary>
         /// "Contains" filter. Applies to: Name, E-mail, URL, Telephone, Text fields.
         /// </summary>
+        [FilterValues(1)]
         Contains,
         /// <summary>
         /// "Does Not Contain" filter. Applies to: Name, E-mail, URL, Telephone, Text fields.
         /// </summary>
         [Description("Does not Contain")]
+        [FilterValues(1)]
         DoesNotContain,
         /// <summary>
         /// "On" filter. Applies to: Date fields.
         /// </summary>
+        [FilterValues(1)]
         On,
         /// <summary>
         /// "At" filter. Applies to: Time fields.
         /// </summary>
+        [FilterValues(1)]
         At,
         /// <summary>
         /// "Equal To" filter. Applies to: Name, Calculation, E-mail, URL, Number, Selection, Sequence, Telephone, Text fields.
         /// </summary>
         [Description("Equal To")]
+        [FilterValues(1)]
         EqualTo,
         /// <summary>
         /// "Not Equal To" filter. Applies to: Name, Calculation, E-mail, URL, Number, Selection, Sequence, Telephone, Text fields.
         /// </summary>
         [Description("Not Equal To")]
+        [FilterValues(1)]
         NotEqualTo,
         /// <summary>
         /// "Less Than" filter. Applies to: Calculation, Number, Sequence number fields.
         /// </summary>
         [Description("Less Than")]
+        [FilterValues(1)]
         LessThan,
         /// <summary>
         /// "Greater Than" filter. Applies to: Calculation, Number, Sequence number fields.
         /// </summary>
         [Description("Greater Than")]
+        [FilterValues(1)]
         GreaterThan,
         /// <summary>
         /// "Between" filter. Applies to: Name, Calculation, Date, E-mail, URL, Number, Sequence number, Time fields.
         /// </summary>
+        [FilterValues(2)]
         Between,
         /// <summary>
         /// "True" filter. Applies to: Checkbox fields.
         /// </summary>
+        [FilterValues(0)]
         True,
         /// <summary>
         /// "False" filter. Applies to: Checkbox fields.
         /// </summary>
+        [FilterValues(0)]
         False,
         /// <summary>
         /// "True" filter. Applies to: Checkbox fields.
         /// </summary>
+        [FilterValues(0)]
         Checked,
         /// <summary>
         /// "False" filter. Applies to: Checkbox fields.
         /// </summary>
         [Description("Not Checked")]
+        [FilterValues(0)]
         NotChecked,
         /// <summary>
         /// "True" filter. Applies to: Checkbox fields.
         /// </summary>
+        [FilterValues(0)]
         Yes,
         /// <summary>
         /// "False" filter. Applies to: Checkbox fields.
         /// </summary>
+        [FilterValues(0)]
         No,
         /// <summary>
         /// "Before" filter. Applies to: Date, Time fields.
         /// </summary>
+        [FilterValues(1)]
         Before,
         /// <summary>
         /// "After" filter. Applies to: Date, Time fields.
         /// </summary>
+        [FilterValues(1)]
         After,
         /// <summary>
         /// "Blank" filter. Applies to Name fields, Date fields, E-mail fields, Telephone fields, URL fields, Time fields.
         /// </summary>
+        [FilterValues(0)]
         Blank,
         /// <summary>
         /// "Shared" filter. Applies to: N/A when used with fields.
         /// </summary>
+        [FilterValues(0)]
         Shared,
         /// <summary>
         /// "Local" filter. Applies to: N/A when used with fields.
         /// </summary>
+        [FilterValues(0)]
         Local,
         /// <summary>
         /// "True" filter. Applies to: Checkbox fields.
         /// </summary>
         [Description("1")]
+        [FilterValues(0)]
         One,
         /// <summary>
         /// "False" filter. Applies to: Checkbox fields.
         /// </summary>
         [Description("0")]
+        [FilterValues(0)]
         Zero
     }
 }
---
+++ CmcLibNet.Database/FilterValuesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vovin.CmcLibNet.Database
+{
+    /// <summary>
+    /// Used to indicate the number of values that a FilterQualifier relies on.
+    /// </summary>
+    /// <remarks>This information can be used when validating an <see cref="ICursorFilter"/></remarks>
+    /// <example>A <see cref="FilterQualifier"/> <code>Checked</code> filter relies on 0 values.
+    /// <para>A <see cref="FilterQualifier"/> <code>Contains</code> filter relies on 1 value.</para>
+    /// <para>A <see cref="FilterQualifier"/> <code>Between</code> filter relies on 2 values.</para>
+    /// </example>
+    [AttributeUsage(AttributeTargets.All)]
+    public class FilterValuesAttribute : Attribute
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="n">Number of dependencies</param>
+        public FilterValuesAttribute(int n)
+        {
+            Number = n;
+        }
+        /// <summary>
+        /// Number of dependencies
+        /// </summary>
+        public int Number { get; private set; }
+    }
+}
