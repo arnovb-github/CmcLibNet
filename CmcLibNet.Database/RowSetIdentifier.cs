02203cd6d8cc3061d6e856e0e67fe662e2e52487
Arno van Boven <arnovanboven@outlook.com>
2019-05-22 23:08:51 +0200

    Made enum Com-visible

--- CmcLibNet.Database/RowSetIdentifier.cs
+++ CmcLibNet.Database/RowSetIdentifier.cs
@@ -1,8 +1,12 @@
-﻿namespace Vovin.CmcLibNet.Database
+﻿using System.Runtime.InteropServices;
+
+namespace Vovin.CmcLibNet.Database
 {
     /// <summary>
     /// Distinguishes GetRowSetBy* parameters
     /// </summary>
+    [ComVisible(true)]
+    [Guid("E5C36BD5-184F-4C66-9BDE-B1CFB65E6C5C")]
     internal enum RowSetIdentifier
     {
         RowId = 0,
