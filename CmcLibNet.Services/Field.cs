1b18a8f691c679eed11e2f9e59a49ac8a545f324
Arno van Boven <arnovanboven@outlook.com>
2020-03-04 23:44:09 +0100

    minor refactor

--- CmcLibNet.Services/Field.cs
+++ CmcLibNet.Services/Field.cs
@@ -3,57 +3,23 @@
     // POCO class to hold info on Commence fields
     internal class Field
     {
-        private string _name = string.Empty;
-        private string _label = string.Empty;
-        private string _value = string.Empty;
-
-        #region Conctructors
+        #region Constructors
         internal Field() { }
 
         internal Field(string fieldName, string fieldLabel, string fieldValue)
         {
-            _name = fieldName;
-            _label = fieldLabel;
-            _value = fieldValue;
+            Name = fieldName;
+            Label = fieldLabel;
+            Value = fieldValue;
         }
         #endregion
 
         #region Properties
-        public string Value
-        {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                _value = value;
-            }
-        }
+        public string Value { get; set; } = string.Empty;
 
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                _name = value;
-            }
-        }
+        public string Name { get; set; } = string.Empty;
 
-        public string Label
-        {
-            get
-            {
-                return _label;
-            }
-            set
-            {
-                _label = value;
-            }
-        }
+        public string Label { get; set; } = string.Empty;
         #endregion
     }
 }
