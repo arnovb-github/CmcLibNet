ac0a1a02000d53164bd121ee458da48942ecfc6b
Arno van Boven <arnovanboven@outlook.com>
2020-04-01 23:47:29 +0200

    Bumped version to 0.8.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyVersion("1.0.0.0")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyVersion("0.8.1.*")]
 //[assembly: AssemblyFileVersion("0.6.*")]
