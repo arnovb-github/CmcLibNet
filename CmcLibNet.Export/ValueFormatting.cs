1965b62b19396882d65bd39e122c04571835fe8d
Arno van Boven <arnovanboven@outlook.com>
2020-04-16 23:29:32 +0200

    Change enum value name

--- CmcLibNet.Export/ValueFormatting.cs
+++ CmcLibNet.Export/ValueFormatting.cs
@@ -10,12 +10,13 @@
         /// </summary>
         None = 0,
         /// <summary>
-        /// Return canonical format as defined by Commence. Unlike Commence, CmcLibNet returns connected data in the format as well. <seealso cref="CmcOptionFlags.Canonical"/>
+        /// Return canonical format as defined by Commence. 
+        /// Unlike Commence, CmcLibNet returns connected data formatted as well. <seealso cref="CmcOptionFlags.Canonical"/>
         /// </summary>
         Canonical = 1,
         /// <summary>
         /// Return data compliant with ISO 8601 format. http://www.iso.org/iso/iso8601
         /// </summary>
-        XSD_ISO8601 = 2,
+        ISO8601 = 2,
     }
 }
